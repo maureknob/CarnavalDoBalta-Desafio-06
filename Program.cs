ef01c3c55a2827f48f8ba29808207f73cad364ea
engenharia@deltafire.com.br <Dev@4000*>
2023-02-22 10:55:25 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+StringBuilder sb = new StringBuilder();
+
+Console.Write("Digite um palavra: ");
+
+string word = Console.ReadLine();
+
+foreach (char c in word)
+{
+    sb.Append((char)((c * c) % 255));
+}
+
+Console.WriteLine(sb.ToString());
